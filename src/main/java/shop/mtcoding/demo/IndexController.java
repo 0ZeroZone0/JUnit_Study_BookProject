d151430c22b101a52afdf7410e7e890fae02a248
0Zone <00zero0zone00@gmail.com>
2023-10-20 20:53:43 +0900

    vscode 세팅 스프링부트 환경

---
+++ src/main/java/shop/mtcoding/demo/IndexController.java
@@ -0,0 +1,21 @@
+package shop.mtcoding.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+public class IndexController {
+    
+    @GetMapping
+    public String index(){
+        log.debug("디버그");
+        log.info("인포");
+        log.warn("경고");
+        log.error("에러");
+        return "index ok";
+
+    }
+}
