20d7f8190715ffd4b9b5d7aa002f3ec0655a8f75
0Zone <00zero0zone00@gmail.com>
2023-10-21 16:52:37 +0900

    vscode 세팅 - gradle 세팅 및 JUnit 세팅

--- src/main/java/shop/mtcoding/demo/IndexController.java
+++ src/main/java/shop/mtcoding/demo/IndexController.java
@@ -1,5 +1,7 @@
 package shop.mtcoding.demo;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,14 +10,14 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 public class IndexController {
-    
-    @GetMapping
-    public String index(){
+
+    @GetMapping("/index")
+    public ResponseEntity<?> index() {
         log.debug("디버그");
         log.info("인포");
         log.warn("경고");
         log.error("에러");
-        return "index ok";
+        return new ResponseEntity<>(HttpStatus.CREATED);
 
     }
 }
---
+++ src/test/java/shop/mtcoding/demo/IndexControllerTest.java
@@ -0,0 +1,31 @@
+package shop.mtcoding.demo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+public class IndexControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void index_test() throws Exception {
+        ResultActions resultActions = mvc.perform(get("/index"));
+
+        resultActions.andExpect(status().isCreated());
+
+        assertThat("기대하는값").isEqualTo("기대하는");
+    }
+
+}
